46db0267808ef8801d07e824ab44b3382701bcbe
Alex Flores <alex.flores@fundacion-jala.org>
2021-07-28 18:25:44 -0400

    session23

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM node:14
-
-COPY [".","/usr/src/"]
-
-WORKDIR /usr/src
-
-RUN npm install
-
-CMD node app.js
---
+++ unit-testing-best-practices/session23/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnitTestingBestPractices;
+namespace session23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StringCalculator.GetEnvironmentVariable("my-fancy-variable"));
+        }
+    }
+}
